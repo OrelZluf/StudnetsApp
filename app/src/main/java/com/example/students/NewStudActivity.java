bdba897308bedb8666927129f70a5190454f9bcd
Orel Zluf <orelz16@gmail.com>
2022-12-11 23:30:20 +0200

    Open the project and added Activities with Studnet class.

---
+++ app/src/main/java/com/example/students/NewStudActivity.java
@@ -0,0 +1,14 @@
+package com.example.students;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewStudActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_stud);
+    }
+}
---
+++ app/src/main/java/com/example/students/Student.java
@@ -0,0 +1,10 @@
+package com.example.students;
+
+public class Student {
+    int id;
+    String name;
+    boolean checkStatus;
+    String image;
+    String phoneNumber;
+    String address;
+}
