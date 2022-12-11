5f0189d5173fb5cd59775000e2801a2f19ec9ef3
Orel Zluf <orelz16@gmail.com>
2022-12-12 00:12:50 +0200

    added list of students and linearlayout in the MainActivity

--- app/src/main/java/com/example/students/MainActivity.java
+++ app/src/main/java/com/example/students/MainActivity.java
@@ -5,13 +5,33 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView studentsList;
+    RecyclerView studentsListRV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Student> studentsList = new ArrayList<>();
+        // add simulation data
+        studentsList.add(new Student("123456",
+                "Orel",
+                "bla", "0505255441", "Beit arif", true));
+        studentsList.add(new Student("123",
+                "Dor",
+                "bla","0505255441", "Beit arif", true));
+        studentsList.add(new Student("1257",
+                "Miki",
+                "bla","0505255441", "Beit arif", true));
+
+        studentsListRV
+                = (RecyclerView)findViewById(
+                R.id.recyclerView);
+
     }
 }
--- app/src/main/java/com/example/students/Student.java
+++ app/src/main/java/com/example/students/Student.java
@@ -1,10 +1,20 @@
 package com.example.students;
 
 public class Student {
-    int id;
+    String id;
     String name;
-    boolean checkStatus;
     String image;
     String phoneNumber;
     String address;
+    boolean checkStatus;
+
+    public Student(String id, String name, String image, String phoneNumber, String address, boolean checkStatus){
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.checkStatus = checkStatus;
+    }
+
 }
